49c927bcb88c597805f4bcf2b670d721aebf3779
seoyoung <ysy1398@gmail.com>
2026-05-12 05:49:53 +0900

    Initial Commit

---
+++ withu/withuApp.swift
@@ -0,0 +1,17 @@
+//
+//  withuApp.swift
+//  withu
+//
+//  Created by seoyoung on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct withuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
